70ae0d2ff33c82cd83168217bbe154e2cbb794ee
luojiaqs <13427521018@163.com>
2017-09-02 17:42:29 +0800

    first commit

--- push.sh
+++ push.sh
@@ -2,5 +2,7 @@
 
 git add ./*
 git commit -m "$1"                                              
+git pull git@github.com:Kap0k/ctf-writeup.git
 git push -u origin master                                                 
+
 
